fde515188c6b4285bf60e2dfbde58a9b59244510
AP Java Period 5 <apjavaperiod5@ac1mwk1agjc9h.78b505.nycboe.org>
2022-10-03 12:09:18 -0400

    added fav movie

---
+++ 01-favorite-movie/Date.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Date here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Date
+{
+    public static void main(String[] args){
+        
+    }
+}
---
+++ 01-favorite-movie/FavoriteMovie.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class FavoriteMovie here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class FavoriteMovie
+{
+    public static void main(String[] args)
+    {
+            System.out.println("My Favorite Movie Is the Lego Movie.");
+    System.out.println("The Lego Movie is my favorite movie because it's very funny.");
+    System.out.println("Another favorite movie of mine is The Amazing Spider-Man");
+    System.out.println("I like the Amazing Spider-Man Because it has a lot of action and Spider-Man is my favorite superhero."); 
+    }
+
+ 
+
+}
